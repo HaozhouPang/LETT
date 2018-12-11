c1356adfa2039fff7594660f6217ebf6f79926d2
HaozhouPang <haozhou@ualberta.ca>
2018-12-11 10:23:57 -0700

    add lines.pkl

--- dataset/mappins/entities_to_linenum.py
+++ dataset/mappins/entities_to_linenum.py
@@ -7,6 +7,9 @@ def main():
 		text = f.readlines()[0]
 		lines = nltk.sent_tokenize(text)
 
+	with open('lines.pkl', 'wb') as f:
+		pickle.dump(lines, f, pickle.HIGHEST_PROTOCOL)
+
 	with open('obama_entities.pkl' , 'rb') as f:
 		entities = pickle.load(f)
 
